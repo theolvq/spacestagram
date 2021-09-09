fd4612aa2c96f38d394fca3a9b06e7b5bbfecde9
daawascript <theo.leveque@gmail.com>
2021-09-09 00:00:13 -0700

    pruned cra, added favicon, basic API request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [APIResponse, setAPIResponse] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [date, setDate] = useState('');
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = 'https://api.nasa.gov/planetary/apod';
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.get(
+        `${URL}?api_key=${API_KEY}&date=${date}`
+      );
+
+      setAPIResponse(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleChange = ({ target }) => {
+    setDate(target.value);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [date]);
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello</h1>
+      <input
+        type='date'
+        name='date'
+        value={date}
+        onChange={({ target }) => {
+          setDate(target.value);
+        }}
+      />
+      <img src={APIResponse.url} alt={APIResponse.title} />
     </div>
   );
 }
