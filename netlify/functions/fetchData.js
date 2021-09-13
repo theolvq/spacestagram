0f331a449d7f8df80f9c5d409077af866a32ed3a
daawascript <theo.leveque@gmail.com>
2021-09-12 23:04:49 -0700

    netlify commit

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -1,3 +1,13 @@
-export const fetchStuff = () => {
-  return "hey";
+const {default: axios} = require("axios");
+
+exports.handler = async function (event, context) {
+  await axios.get(
+    `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}`
+  );
+  return {
+    statusCode: 200,
+    event,
+    context,
+    body: {message: "hello, world"},
+  };
 };
